d4eed0e1f3c77f8fc85c9a8d0f53a946e7cbe5ab
Zebediah Figura <z.figura12@gmail.com>
2020-08-31 23:38:09 -0500

    ntdll: Reimplement the critical section fast path on top of Win32 futexes.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -1569,9 +1569,6 @@ static struct unix_funcs unix_funcs =
 #endif
     DbgUiIssueRemoteBreakin,
     RtlGetSystemTimePrecise,
-    fast_RtlpWaitForCriticalSection,
-    fast_RtlpUnWaitCriticalSection,
-    fast_RtlDeleteCriticalSection,
     fast_RtlTryAcquireSRWLockExclusive,
     fast_RtlAcquireSRWLockExclusive,
     fast_RtlTryAcquireSRWLockShared,
