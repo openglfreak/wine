c19e121e564706274399d8a9692313106d47cf2a
Zebediah Figura <z.figura12@gmail.com>
2020-11-02 20:24:07 -0600

    ntdll: Reimplement Win32 futexes on top of thread-ID alerts.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -1569,9 +1569,6 @@ static struct unix_funcs unix_funcs =
 #endif
     DbgUiIssueRemoteBreakin,
     RtlGetSystemTimePrecise,
-    RtlWaitOnAddress,
-    RtlWakeAddressAll,
-    RtlWakeAddressSingle,
     fast_RtlpWaitForCriticalSection,
     fast_RtlpUnWaitCriticalSection,
     fast_RtlDeleteCriticalSection,
